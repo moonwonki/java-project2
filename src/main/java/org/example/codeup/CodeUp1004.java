a35703db26a916139acb4ee0ec9b8a9b3ccc0228
moonwonki <moonwonki1999@gmail.com>
2023-04-19 18:42:04 +0900

    feat: CodeUp1001~1005 추가

---
+++ src/main/java/org/example/codeup/CodeUp1004.java
@@ -0,0 +1,7 @@
+package org.example.codeup;
+
+public class CodeUp1004 {
+    public static void main(String[] args) {
+        System.out.println("\'Hello\'");
+    }
+}
---
+++ src/main/java/org/example/codeup/CodeUp1005.java
@@ -0,0 +1,8 @@
+package org.example.codeup;
+
+public class CodeUp1005 {
+
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }
+}
