3f5729b60a70adc0a075fc496f43a368e50da380
moonwonki <moonwonki1999@gmail.com>
2023-04-19 13:10:07 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/org/example/CrawlDate.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
