5d321fe01a9065a0d9aa9d55c36eeffabc9fcd66
moonwonki <moonwonki1999@gmail.com>
2023-04-19 11:30:54 +0900

    feat: Bye 기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/org/example/ByeClass.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ByeClass {
+    public void print(){
+        System.out.println("Bye!");
+    }
+
+}
---
+++ src/main/java/org/example/ByeClassTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass byeClass = new ByeClass(); // 인스턴스화
+        byeClass.print();
+    }
+}
