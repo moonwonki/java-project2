605b3c88edccfe7208e20c59a240f551960a2ada
moonwonki <moonwonki1999@gmail.com>
2023-04-20 11:48:24 +0900

    feat: PrintfDate 추가

---
+++ src/main/java/org/example/week1/day4/PrintfDate.java
@@ -0,0 +1,11 @@
+package org.example.week1.day4;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 4);
+        System.out.printf("%02d\n", 4);
+        System.out.printf("%02d\n", 11);
+        System.out.printf("%d-%02d-%02d", 2023, 4, 20);
+
+    }
+}
