a1d4a2f233a55e19e8a9295b541abe81d1d2b066
moonwonki <moonwonki1999@gmail.com>
2023-04-20 13:13:22 +0900

    fix: MultiplicationTable 수정

--- src/main/java/org/example/week1/day4/MultiplicationTable.java
+++ src/main/java/org/example/week1/day4/MultiplicationTable.java
@@ -4,5 +4,6 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         System.out.println("2" + "*" + "1" + "=" + "2");
         System.out.printf("%d * %d = %d", 2, 2, 4);
+        System.out.printf("%d * %d = %d", 2, 3, 2 * 3);
     }
 }
