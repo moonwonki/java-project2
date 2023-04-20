87ad2cda49fa50c3cbba001d1df99be3ad6248c8
moonwonki <moonwonki1999@gmail.com>
2023-04-20 11:29:00 +0900

    feat: MultiplicationTable 추가

---
+++ src/main/java/org/example/week1/day4/MultiplicationTable.java
@@ -0,0 +1,8 @@
+package org.example.week1.day4;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("2" + "*" + "1" + "=" + "2");
+        System.out.printf("%d * %d = %d", 2, 2, 4);
+    }
+}
