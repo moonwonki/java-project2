bb0340a75aa19dad026f6c88bbdcb46c443571fc
moonwonki <moonwonki1999@gmail.com>
2023-04-20 13:20:35 +0900

    fix: MultiplicationTable 수정 - 숫자 변경

--- src/main/java/org/example/week1/day4/MultiplicationTable.java
+++ src/main/java/org/example/week1/day4/MultiplicationTable.java
@@ -2,8 +2,16 @@ package org.example.week1.day4;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        System.out.println("2" + "*" + "1" + "=" + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 4);
-        System.out.printf("%d * %d = %d", 2, 3, 2 * 3);
+        int num = 2;
+        System.out.printf("%d * %d = %d\n", num, 1, num * 1);
+        System.out.printf("%d * %d = %d\n", num, 2, num * 2);
+        System.out.printf("%d * %d = %d\n", num, 3, num * 3);
+        System.out.printf("%d * %d = %d\n", num, 4, num * 4);
+        System.out.printf("%d * %d = %d\n", num, 5, num * 5);
+        System.out.printf("%d * %d = %d\n", num, 6, num * 6);
+        System.out.printf("%d * %d = %d\n", num, 7, num * 7);
+        System.out.printf("%d * %d = %d\n", num, 8, num * 8);
+        System.out.printf("%d * %d = %d\n", num, 9, num * 9);
+
     }
 }
