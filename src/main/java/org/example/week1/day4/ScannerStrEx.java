01daab93ead2d8bfb2c2caabecc00dbda84f4f9e
moonwonki <moonwonki1999@gmail.com>
2023-04-20 09:39:50 +0900

    feat: 문자열 2개 입력받아 더하기 추가 - ScannerStrEx 추가 - ScannerStrExTest 추가

---
+++ src/main/java/org/example/week1/day4/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package org.example.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void inputStringAndPlus(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/org/example/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package org.example.week1.day4;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringAndPlus();
+    }
+}
