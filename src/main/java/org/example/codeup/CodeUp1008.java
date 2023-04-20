fad310be1bf5709590873bbadd5ca98cea9f267c
moonwonki <moonwonki1999@gmail.com>
2023-04-20 11:33:44 +0900

    feat: CodeUp1010 추가 - CodeUp1008 Unicode 관련 문제로 한글 지움. - CodeUp1010 추가

--- src/main/java/org/example/codeup/CodeUp1008.java
+++ src/main/java/org/example/codeup/CodeUp1008.java
@@ -1,9 +1,17 @@
 package org.example.codeup;
 
 public class CodeUp1008 {
-    //현재 본인 환경에서 java와 인텔리제이의 호환성 문제로 인해 unicode가 ???로 출력된다.
+    // unicode error in IDE. used two \\ for escape.
+
     public static void main(String[] args) {
-        String str = "\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518";
+
+        String str = "\\u250C\\u252C\\u2510\n\\u251C\\u253C\\u2524\n\\u2514\\u2534\\u2518";
         System.out.println(str);
+
+
     }
+
+
 }
+
+
