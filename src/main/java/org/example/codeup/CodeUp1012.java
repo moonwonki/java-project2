aeb9ee45fb6a5f62d8803e8e2230f5c7cb65e653
moonwonki <moonwonki1999@gmail.com>
2023-04-20 13:40:22 +0900

    feat: CodeUp1011~1012 추가

---
+++ src/main/java/org/example/codeup/CodeUp1012.java
@@ -0,0 +1,12 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1012 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.printf("%f", scn.nextFloat());
+    }
+}
