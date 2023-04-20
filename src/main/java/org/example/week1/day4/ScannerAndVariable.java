ebd2b91f8abca87ac60f59a21249e05414b76f35
moonwonki <moonwonki1999@gmail.com>
2023-04-20 15:32:24 +0900

    feat: ScannerAndVariable 추가

---
+++ src/main/java/org/example/week1/day4/ScannerAndVariable.java
@@ -0,0 +1,14 @@
+package org.example.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerAndVariable {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int val1 = scn.nextInt();
+        int val2 = scn.nextInt();
+
+        System.out.printf("%d", val1 + val2);
+    }
+}
