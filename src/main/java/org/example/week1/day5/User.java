0b269331f3195fef2bd94204627496a06e5c3f46
moonwonki <moonwonki1999@gmail.com>
2023-04-21 15:15:51 +0900

    feat: User, UserTest 추가

---
+++ src/main/java/org/example/week1/day5/User.java
@@ -0,0 +1,12 @@
+package org.example.week1.day5;
+
+public class User {
+    //name
+    String name;
+
+    //phone number
+    String phoneNumber;
+
+    //age
+    int age;
+}
---
+++ src/main/java/org/example/week1/day5/UserTest.java
@@ -0,0 +1,18 @@
+package org.example.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User moonwonki = new User();
+        moonwonki.name = "Moon Won Ki";
+        moonwonki.phoneNumber = "010-0000-0000";
+        moonwonki.age = 25;
+
+        User mooninkyoung = new User();
+        mooninkyoung.name = "Moon In Kyoung";
+        mooninkyoung.phoneNumber = "111";
+        mooninkyoung.age = 20;
+    }
+
+
+
+}
