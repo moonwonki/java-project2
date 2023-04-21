3a966cfb8e4d0d551e9616e789c26bdcff9b1cd4
moonwonki <moonwonki1999@gmail.com>
2023-04-21 14:10:31 +0900

    feat: CodeUp1019 추가

---
+++ src/main/java/org/example/codeup/CodeUp1019.java
@@ -0,0 +1,16 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1019 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+        String stringInput[] = scn.nextLine().split("\\.");
+
+        System.out.printf("%04d.%02d.%02d", Integer.parseInt(stringInput[0]), Integer.parseInt(stringInput[1]), Integer.parseInt(stringInput[2]));
+
+
+    }
+}
