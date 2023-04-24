5656762cfd31eeeed27e7f2f1bdd006e1854bb33
moonwonki <moonwonki1999@gmail.com>
2023-04-24 11:14:58 +0900

    feat: FloatAndDoubles 추가

---
+++ src/main/java/org/example/week2/day1/FloatAndDoubles.java
@@ -0,0 +1,22 @@
+package org.example.week2.day1;
+
+public class FloatAndDoubles {
+    public static void main(String[] args) {
+
+        float f1 = 3.14f;
+        float f2 = 1.23e10f;
+
+        double d1 = 3.141592653589793;
+        double d2 = 1.23e100;
+
+        float result1 = f1 * 2.0f;
+        double result2 = d1 / 2.0;
+
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+        System.out.println("d1 = " + d1);
+        System.out.println("d2 = " + d2);
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+    }
+}
