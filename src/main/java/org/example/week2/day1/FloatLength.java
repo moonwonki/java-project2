02e70cf837502c763076bfda9be613906d08cb05
moonwonki <moonwonki1999@gmail.com>
2023-04-24 11:30:00 +0900

    feat: FloatLength 추가

---
+++ src/main/java/org/example/week2/day1/FloatLength.java
@@ -0,0 +1,10 @@
+package org.example.week2.day1;
+
+public class FloatLength {
+    public static void main(String[] args) {
+        float f1 = 1.12345678f;
+        double d1 = 1.1234567890123456789012345678901234567890;
+        System.out.println(f1); // 1.1234568
+        System.out.println(d1); // 1.1234567890123457
+    }
+}
