a81fffbe398eaa01bcdafd05333d25c7f9fc350c
moonwonki <moonwonki1999@gmail.com>
2023-04-24 10:05:46 +0900

    feat: DefineAVariable, VariableEx 추가

---
+++ src/main/java/org/example/week2/day1/DefineAVariable.java
@@ -0,0 +1,14 @@
+package org.example.week2.day1;
+
+public class DefineAVariable {
+    public static void main(String[] args) {
+        int intOne= 0;
+
+        String myName = "Moon";
+        String myAnotherName = new String("Moon Won Ki");
+
+        float floatOne = 1.0f;
+
+
+    }
+}
---
+++ src/main/java/org/example/week2/day1/VariableEx.java
@@ -0,0 +1,11 @@
+package org.example.week2.day1;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        String stringOne = new String("1");
+        int intOne = 1;
+        float floatOne = 1.0f;
+
+        System.out.printf("String : %s, Integer : %d, float : %f", stringOne, intOne, floatOne);
+    }
+}
