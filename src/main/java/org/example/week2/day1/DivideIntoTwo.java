b9c784812994a9f60d613748b8525ca9e89c854a
moonwonki <moonwonki1999@gmail.com>
2023-04-24 10:17:17 +0900

    feat: DivideIntoTwo 추가

---
+++ src/main/java/org/example/week2/day1/DivideIntoTwo.java
@@ -0,0 +1,16 @@
+package org.example.week2.day1;
+
+public class DivideIntoTwo {
+    public static void main(String[] args) {
+        //1 + 2 / 1
+        float floatOne = 1;
+        float floatTwo = 2;
+
+        System.out.printf("%f\n", floatOne + floatOne / floatTwo);
+
+        int intOne = 1;
+        int intTwo = 2;
+
+        System.out.printf("%d\n", intOne + intOne / intTwo);
+    }
+}
