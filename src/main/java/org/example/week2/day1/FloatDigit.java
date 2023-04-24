cb6a5c93a5533685501ebb17d89e59f363f033f0
moonwonki <moonwonki1999@gmail.com>
2023-04-24 11:20:46 +0900

    feat: FloatDigit 추가

---
+++ src/main/java/org/example/week2/day1/FloatDigit.java
@@ -0,0 +1,12 @@
+package org.example.week2.day1;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f5 = 1.23e5f;
+        float f6 = 1.23e6f;
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+
+        System.out.printf("%f\n%f\n%f\n%f\n", f5,f6,f7,f8);
+    }
+}
