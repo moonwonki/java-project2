02b0cce90b7af6bb00526bf22090327fbe188ff6
moonwonki <moonwonki1999@gmail.com>
2023-04-24 14:34:46 +0900

    feat: BooleanEx 추가

---
+++ src/main/java/org/example/week2/day1/BooleanEx.java
@@ -0,0 +1,13 @@
+package org.example.week2.day1;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean firstFlag = false;
+        boolean secondFlag = true;
+
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println("isBalanceSufficient = " + isBalanceSufficient);
+    }
+}
