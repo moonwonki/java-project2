83650dde594b0a624b42bfec82c8ef3787a4bccc
moonwonki <moonwonki1999@gmail.com>
2023-04-25 14:19:01 +0900

    feat: added DefAndInitArr, StudentMathScore

---
+++ src/main/java/org/example/week2/day2/DefAndInitArr.java
@@ -0,0 +1,13 @@
+package org.example.week2.day2;
+
+public class DefAndInitArr {
+    public static void main(String[] args) {
+        int[] iArr = new int[5];
+        iArr[0] = 1;
+        iArr[1] = 2;
+        iArr[2] = 3;
+        iArr[3] = 4;
+        iArr[4] = 5;
+        System.out.println(iArr);
+    }
+}
---
+++ src/main/java/org/example/week2/day2/StudentMathScore.java
@@ -0,0 +1,15 @@
+package org.example.week2.day2;
+
+public class StudentMathScore {
+    public static void main(String[] args) {
+        // inefficient!
+        int student1MathScore = 100;
+        int student2MathScore = 100;
+        int student3MathScore = 100;
+
+
+        //efficient!
+        int[] studentMathScoresArr = new int[30];
+
+    }
+}
