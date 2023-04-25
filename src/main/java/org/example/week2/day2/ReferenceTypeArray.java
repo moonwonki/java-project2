50b0b9fbf6f3db3867768ed3426c5d8faf465d19
moonwonki <moonwonki1999@gmail.com>
2023-04-25 14:51:04 +0900

    fix: added ReferenceTypeArray

---
+++ src/main/java/org/example/week2/day2/ReferenceTypeArray.java
@@ -0,0 +1,12 @@
+package org.example.week2.day2;
+
+import java.util.Arrays;
+
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        float[] fArr = new float[3];
+        String[] sArr = new String[10];
+        System.out.println(Arrays.toString(fArr));
+        System.out.println(Arrays.toString(sArr));
+    }
+}
