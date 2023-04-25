64befc081682fbddab116eea4733a34fbb07470a
moonwonki <moonwonki1999@gmail.com>
2023-04-25 15:28:17 +0900

    feat: added CodeUp1026~1027

---
+++ src/main/java/org/example/codeup/CodeUp1027.java
@@ -0,0 +1,15 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1027 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        String inputString = scn.nextLine();
+
+        String[] splitInputString = inputString.split("\\.");
+
+        System.out.printf("%s-%s-%s", splitInputString[2], splitInputString[1], splitInputString[0]);
+    }
+}
