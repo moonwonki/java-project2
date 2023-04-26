b4094232f1f38c6eb7ffd4b518670afb0eec7b98
moonwonki <moonwonki1999@gmail.com>
2023-04-26 10:38:26 +0900

    fix: fixed TenXTen

--- src/main/java/org/example/week2/day3/TenXTen.java
+++ src/main/java/org/example/week2/day3/TenXTen.java
@@ -6,19 +6,13 @@ public class TenXTen {
     public static void main(String[] args) {
 
         int[][] arr10x10 = new int[10][10]; // 배열 선언과 초기화
-        System.out.println(Arrays.toString(arr10x10[0]));
-        System.out.println(Arrays.toString(arr10x10[1]));
-        System.out.println(Arrays.toString(arr10x10[2]));
-        System.out.println(Arrays.toString(arr10x10[3]));
-        System.out.println(Arrays.toString(arr10x10[4]));
-        System.out.println(Arrays.toString(arr10x10[5]));
-        System.out.println(Arrays.toString(arr10x10[6]));
-        System.out.println(Arrays.toString(arr10x10[7]));
-        System.out.println(Arrays.toString(arr10x10[8]));
-        System.out.println(Arrays.toString(arr10x10[9]));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
+
 
         int[][] arr3x3 = new int[3][3];
         arr3x3[0][1] = 1;
-        
+
     }
 }
