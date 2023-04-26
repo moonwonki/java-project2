461b2e2007196d723f8a0dac77ed427733893c7c
moonwonki <moonwonki1999@gmail.com>
2023-04-26 10:32:11 +0900

    feat: added TenXTen

---
+++ src/main/java/org/example/week2/day3/TenXTen.java
@@ -0,0 +1,24 @@
+package org.example.week2.day3;
+
+import java.util.Arrays;
+
+public class TenXTen {
+    public static void main(String[] args) {
+
+        int[][] arr10x10 = new int[10][10]; // 배열 선언과 초기화
+        System.out.println(Arrays.toString(arr10x10[0]));
+        System.out.println(Arrays.toString(arr10x10[1]));
+        System.out.println(Arrays.toString(arr10x10[2]));
+        System.out.println(Arrays.toString(arr10x10[3]));
+        System.out.println(Arrays.toString(arr10x10[4]));
+        System.out.println(Arrays.toString(arr10x10[5]));
+        System.out.println(Arrays.toString(arr10x10[6]));
+        System.out.println(Arrays.toString(arr10x10[7]));
+        System.out.println(Arrays.toString(arr10x10[8]));
+        System.out.println(Arrays.toString(arr10x10[9]));
+
+        int[][] arr3x3 = new int[3][3];
+        arr3x3[0][1] = 1;
+        
+    }
+}
