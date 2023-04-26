84518986fbb084b02881a2b770ec1ea75bd4daa8
moonwonki <moonwonki1999@gmail.com>
2023-04-26 13:37:23 +0900

    feat: added ArrLength

---
+++ src/main/java/org/example/week2/day3/ArrLength.java
@@ -0,0 +1,8 @@
+package org.example.week2.day3;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arrInteger = new int[5];
+        System.out.println(arrInteger.length);
+    }
+}
