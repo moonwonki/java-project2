d9e26e84247d09cd75a7086cbbc856cc49406126
moonwonki <moonwonki1999@gmail.com>
2023-04-26 10:49:39 +0900

    feat: fixed TenXTen, added DefineArr2

---
+++ src/main/java/org/example/week2/day3/DefineArr2.java
@@ -0,0 +1,11 @@
+package org.example.week2.day3;
+
+public class DefineArr2 {
+    public static void main(String[] args) {
+        int iArr[] = new int[] {1,2,3,4};
+
+        System.out.println(iArr[0]);
+        System.out.println(iArr[1]);
+
+    }
+}
--- src/main/java/org/example/week2/day3/TenXTen.java
+++ src/main/java/org/example/week2/day3/TenXTen.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TenXTen {
     public static void main(String[] args) {
 
-        int[][] arr10x10 = new int[10][10]; // 배열 선언과 초기화
+        int[][] arr10x10 = new int[10][10];
         for (int i = 0; i < 10; i++) {
             System.out.println(Arrays.toString(arr10x10[i]));
         }
