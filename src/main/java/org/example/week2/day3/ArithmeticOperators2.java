418aa247246b1c7d66d9e8a041ef4619051f13a0
moonwonki <moonwonki1999@gmail.com>
2023-04-26 14:09:38 +0900

    feat: added ArithmeticOperators2

---
+++ src/main/java/org/example/week2/day3/ArithmeticOperators2.java
@@ -0,0 +1,14 @@
+package org.example.week2.day3;
+
+public class ArithmeticOperators2 {
+    public static void main(String[] args) {
+        int aNumber = 10;
+        int bNumber = 3;
+        System.out.println(10 + bNumber);
+        System.out.println(aNumber - 3);
+        System.out.println(aNumber * bNumber);
+        System.out.println(10 * 3 / bNumber);
+        System.out.println(49 % bNumber);
+    }
+}
+
