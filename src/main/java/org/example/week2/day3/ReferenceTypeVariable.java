fa42190c3dd5ffbefa293e61ed0e05fbbd42f31c
moonwonki <moonwonki1999@gmail.com>
2023-04-26 09:27:54 +0900

    feat: added ReferenceTypeVariable, Student

---
+++ src/main/java/org/example/week2/day3/ReferenceTypeVariable.java
@@ -0,0 +1,11 @@
+package org.example.week2.day3;
+
+public class ReferenceTypeVariable {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        Student[] students = new Student[30];
+
+
+    }
+}
---
+++ src/main/java/org/example/week2/day3/Student.java
@@ -0,0 +1,7 @@
+package org.example.week2.day3;
+
+public class Student {
+    String name;
+    String phoneNumber;
+    int age;
+}
