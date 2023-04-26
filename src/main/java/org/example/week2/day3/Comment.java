9852cc2c10540740901a77e32261319a5f11f046
moonwonki <moonwonki1999@gmail.com>
2023-04-26 10:28:08 +0900

    feat: added Comment, Post

---
+++ src/main/java/org/example/week2/day3/Comment.java
@@ -0,0 +1,5 @@
+package org.example.week2.day3;
+
+public class Comment {
+    String content;
+}
---
+++ src/main/java/org/example/week2/day3/Post.java
@@ -0,0 +1,7 @@
+package org.example.week2.day3;
+
+public class Post {
+    String title;
+    String content;
+    Comment comments[];
+}
