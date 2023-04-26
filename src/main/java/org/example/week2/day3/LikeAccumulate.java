3f63b655b97b5e5df2d6c4f595af3aae77959e8d
moonwonki <moonwonki1999@gmail.com>
2023-04-26 15:35:31 +0900

    feat: added LikeAccumulate

---
+++ src/main/java/org/example/week2/day3/LikeAccumulate.java
@@ -0,0 +1,15 @@
+package org.example.week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount - 1;
+        System.out.println(likeCount);
+    }
+}
