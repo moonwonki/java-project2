df8de08999469ee45942744ebfd127d2740277a4
moonwonki <moonwonki1999@gmail.com>
2023-04-26 15:46:27 +0900

    feat: added StringAccu

---
+++ src/main/java/org/example/week2/day3/StringAccu.java
@@ -0,0 +1,12 @@
+package org.example.week2.day3;
+
+public class StringAccu {
+    public static void main(String[] args) {
+        String answer = "";
+        answer = answer + "BEEEEEH";
+        answer = answer + "-17";
+        answer = answer + " BAUMMER";
+
+        System.out.println(answer);
+    }
+}
