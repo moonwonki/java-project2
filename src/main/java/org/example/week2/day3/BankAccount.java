bda8b924f03f4325491ff5aaf65e3614f58d65cb
moonwonki <moonwonki1999@gmail.com>
2023-04-26 15:37:17 +0900

    feat: added BankAccount

---
+++ src/main/java/org/example/week2/day3/BankAccount.java
@@ -0,0 +1,12 @@
+package org.example.week2.day3;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+
+    }
+}
