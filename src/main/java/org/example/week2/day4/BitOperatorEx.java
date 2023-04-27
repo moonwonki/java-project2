ebe4bab6bbf4398663e1cf9a2c61da73c1f4b9fd
moonwonki <moonwonki1999@gmail.com>
2023-04-27 14:22:31 +0900

    feat: BitOperatorEx

---
+++ src/main/java/org/example/week2/day4/BitOperatorEx.java
@@ -0,0 +1,15 @@
+package org.example.week2.day4;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int iVal1 = 1;
+
+        System.out.println(iVal1 << 1);
+        System.out.println(iVal1 << 2);
+
+        int iVal2 = 32;
+
+        System.out.println(iVal2 >> 1);
+        System.out.println(iVal2 >> 2);
+    }
+}
