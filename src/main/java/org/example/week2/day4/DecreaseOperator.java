5659bed5a57c558c781b930765dcbaf0a4c61f7f
moonwonki <moonwonki1999@gmail.com>
2023-04-27 10:07:50 +0900

    feat: added DecreaseOperator

---
+++ src/main/java/org/example/week2/day4/DecreaseOperator.java
@@ -0,0 +1,9 @@
+package org.example.week2.day4;
+
+public class DecreaseOperator {
+    public static void main(String[] args) {
+        int iVal = 10;
+        System.out.println(iVal--);
+        System.out.println(iVal);
+    }
+}
