cde30769174f9567ecc89c88bad449b99611d930
moonwonki <moonwonki1999@gmail.com>
2023-04-27 10:37:31 +0900

    feat: added ComparisonOperatorsEx

---
+++ src/main/java/org/example/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,11 @@
+package org.example.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+
+        System.out.printf("iVal1 == iVal2 = %b", iVal1 == iVal2);
+        System.out.printf("iVal1 != iVal2 = %b", iVal1 != iVal2);
+    }
+}
