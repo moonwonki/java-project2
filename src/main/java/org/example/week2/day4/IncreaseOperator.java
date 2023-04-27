40e2d7a92291344044b1ff3e4192167840758df4
moonwonki <moonwonki1999@gmail.com>
2023-04-27 10:05:12 +0900

    feat: added IncreaseOperator

---
+++ src/main/java/org/example/week2/day4/IncreaseOperator.java
@@ -0,0 +1,12 @@
+package org.example.week2.day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+
+        int iVal = 10;
+        System.out.println(iVal++);
+
+        iVal = 10;
+        System.out.println(++iVal);
+    }
+}
