08f0d755a171337f13f8e8467de7918989236f37
moonwonki <moonwonki1999@gmail.com>
2023-04-27 13:21:21 +0900

    feat: CodeUp1053~1055 added

---
+++ src/main/java/org/example/codeup/CodeUp1053.java
@@ -0,0 +1,13 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1053 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int oneZeroInput = scn.nextInt();
+        if (oneZeroInput == 1) System.out.println("0");
+        else if (oneZeroInput == 0) System.out.println("1");
+    }
+}
---
+++ src/main/java/org/example/codeup/CodeUp1054.java
@@ -0,0 +1,15 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1054 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int firstInput = scn.nextInt();
+        int secondInput = scn.nextInt();
+
+        if (firstInput == 1 && secondInput == 1) System.out.println("1");
+        else System.out.println("0");
+    }
+}
