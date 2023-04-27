454307c92a198084da70e04f27f8f5eba51ceefe
moonwonki <moonwonki1999@gmail.com>
2023-04-27 15:05:16 +0900

    feat: CodeUp1059 ~ 1062

---
+++ src/main/java/org/example/codeup/CodeUp1059.java
@@ -0,0 +1,12 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1059 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int numInput = scn.nextInt();
+
+        System.out.println(~numInput);
+    }
+}
---
+++ src/main/java/org/example/codeup/CodeUp1062.java
@@ -0,0 +1,14 @@
+package org.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1062 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int firstInput = scn.nextInt();
+        int secondInput = scn.nextInt();
+
+        System.out.println(firstInput ^ secondInput);
+    }
+}
