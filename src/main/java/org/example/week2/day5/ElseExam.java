d1d252d4ecd795f58b5a542a87a8307f5932811c
moonwonki <moonwonki1999@gmail.com>
2023-04-28 09:18:29 +0900

    feat: ElseExam, ElseExam2

---
+++ src/main/java/org/example/week2/day5/ElseExam.java
@@ -0,0 +1,23 @@
+package org.example.week2.day5;
+
+public class ElseExam {
+    public static void main(String[] args) {
+        boolean checkTrue = true;
+
+        if (checkTrue){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("not true");
+        }
+
+        boolean checkFalse = false;
+
+        if (checkFalse) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
---
+++ src/main/java/org/example/week2/day5/ElseExam2.java
@@ -0,0 +1,13 @@
+package org.example.week2.day5;
+
+public class ElseExam2 {
+    public static void main(String[] args) {
+        int account = 1400;
+        if (account >= 1500){
+            System.out.println("approved");
+        }
+        else {
+            System.out.println("you don't have enough money!");
+        }
+    }
+}
