ebcd11e1ecaddde9da82025b502e9877f531f3fb
moonwonki <moonwonki1999@gmail.com>
2023-04-28 09:45:55 +0900

    feat: OddEven

---
+++ src/main/java/org/example/week2/day5/OddEven.java
@@ -0,0 +1,13 @@
+package org.example.week2.day5;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int num = 1;
+        if (num % 2 == 0){
+            System.out.println("even!");
+        }
+        else {
+            System.out.println("odd!");
+        }
+    }
+}
