40f3de10262ac3542a8edb828fd90bff2d737ea2
moonwonki <moonwonki1999@gmail.com>
2023-04-28 10:16:45 +0900

    feat: IfWrong

---
+++ src/main/java/org/example/week2/day5/IfWrong.java
@@ -0,0 +1,17 @@
+package org.example.week2.day5;
+
+public class IfWrong {
+    public static void main(String[] args) {
+        int score = 81;
+
+        if (score >= 90){
+            System.out.println("A");
+        }
+        if (score >= 80){
+            System.out.println("B");
+        }
+        if (score >= 70){
+            System.out.println("C");
+        }
+    }
+}
