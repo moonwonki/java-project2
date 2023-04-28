88715f2d83a9018dfe09ca02676803799ca2278c
moonwonki <moonwonki1999@gmail.com>
2023-04-28 09:09:16 +0900

    feat: ifEx, IfComparisonOperation

---
+++ src/main/java/org/example/week2/day5/IfComparisonOperation.java
@@ -0,0 +1,16 @@
+package org.example.week2.day5;
+
+public class IfComparisonOperation {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean isAdult = age >= 18;
+
+        if (isAdult) {
+            System.out.println("he or she is adult");
+        }
+
+        if (age < 18){
+            System.out.println("he or she is not adult");
+        }
+    }
+}
---
+++ src/main/java/org/example/week2/day5/ifEx.java
@@ -0,0 +1,9 @@
+package org.example.week2.day5;
+
+public class ifEx {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("always");
+        }
+    }
+}
