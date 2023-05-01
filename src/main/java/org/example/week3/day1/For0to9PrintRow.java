43a3af37c76d8d58801bcd68ab2593b955a71f36
moonwonki <moonwonki1999@gmail.com>
2023-05-01 10:18:02 +0900

    feat : for0to9PrintRow

---
+++ src/main/java/org/example/week3/day1/For0to9PrintRow.java
@@ -0,0 +1,10 @@
+package org.example.week3.day1;
+
+public class For0to9PrintRow {
+    public static void main(String[] args) {
+        for (int loopVar = 0; loopVar <= 9; loopVar++ ){
+
+            System.out.printf("%d ", loopVar);
+        }
+    }
+}
