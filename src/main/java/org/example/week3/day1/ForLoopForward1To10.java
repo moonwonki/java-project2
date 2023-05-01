aea4b15a7b8e8eea2389ca8e2c25a06cecae682c
moonwonki <moonwonki1999@gmail.com>
2023-05-01 11:19:14 +0900

    feat : ForLoopForward1To10

---
+++ src/main/java/org/example/week3/day1/ForLoopForward1To10.java
@@ -0,0 +1,9 @@
+package org.example.week3.day1;
+
+public class ForLoopForward1To10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
