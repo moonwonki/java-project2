54fba6aacb367cd54055f6fc045b674b810ce489
moonwonki <moonwonki1999@gmail.com>
2023-05-01 10:09:15 +0900

    feat : for0to9

---
+++ src/main/java/org/example/week3/day1/for0to9.java
@@ -0,0 +1,9 @@
+package org.example.week3.day1;
+
+public class for0to9 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++){
+            System.out.println(i);
+        }
+    }
+}
