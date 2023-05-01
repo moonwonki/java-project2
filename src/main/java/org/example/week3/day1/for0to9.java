529a68330c0e58455aeb82dd11159eaca18a4702
moonwonki <moonwonki1999@gmail.com>
2023-05-01 10:09:58 +0900

    fix : for0to9 (distinct variable name)

--- src/main/java/org/example/week3/day1/for0to9.java
+++ src/main/java/org/example/week3/day1/for0to9.java
@@ -2,8 +2,8 @@ package org.example.week3.day1;
 
 public class for0to9 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 9; i++){
-            System.out.println(i);
+        for (int loopVar = 0; loopVar <= 9; loopVar++){
+            System.out.println(loopVar);
         }
     }
 }
