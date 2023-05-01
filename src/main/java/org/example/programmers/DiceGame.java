3e2b5c456c8a136fcbbdb265fe0a940fa768d522
moonwonki <moonwonki1999@gmail.com>
2023-05-01 10:51:03 +0900

    feat : 주사위 게임 추가

---
+++ src/main/java/org/example/programmers/DiceGame.java
@@ -0,0 +1,19 @@
+package org.example.programmers;
+
+public class DiceGame {
+
+    class Solution {
+        public int solution(int[] num_list) {
+            int answer = 0;
+            int oddNum = 0;
+            int evenNum = 0;
+            for (int i = 0; i < num_list.length; i++){
+                if (num_list[i] % 2 == 0) evenNum = evenNum * 10 + num_list[i];
+                else oddNum = oddNum * 10 + num_list[i];
+            }
+
+            answer = oddNum + evenNum;
+            return answer;
+        }
+    }
+}
