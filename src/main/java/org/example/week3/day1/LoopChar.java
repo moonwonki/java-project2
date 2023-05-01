0c239f7aa15a7d8338e3e5b83f8f3cdb4576c9df
moonwonki <moonwonki1999@gmail.com>
2023-05-01 13:41:01 +0900

    feat : LoopChar

---
+++ src/main/java/org/example/week3/day1/LoopChar.java
@@ -0,0 +1,15 @@
+package org.example.week3.day1;
+
+public class LoopChar {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'e'; c++){
+            System.out.println(c);
+        }
+
+        for (char c = 97; c <= 101; c++){
+            System.out.println(c);
+        }
+
+
+    }
+}
