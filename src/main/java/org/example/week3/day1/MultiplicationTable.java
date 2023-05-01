a9aeea2b9696ba55a2ba93564df2f02f1e39a301
moonwonki <moonwonki1999@gmail.com>
2023-05-01 14:18:00 +0900

    feat : MultiplicationTable

---
+++ src/main/java/org/example/week3/day1/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package org.example.week3.day1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++){
+            System.out.printf("2 * %d = %d\n", i, 2*i);
+        }
+    }
+}
