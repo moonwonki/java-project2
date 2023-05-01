e3731cbef70f7911d4827b87d2f7533777bdb716
moonwonki <moonwonki1999@gmail.com>
2023-05-01 14:28:15 +0900

    feat : MultiplicationTable added 3, 4 table

--- src/main/java/org/example/week3/day1/MultiplicationTable.java
+++ src/main/java/org/example/week3/day1/MultiplicationTable.java
@@ -2,8 +2,19 @@ package org.example.week3.day1;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        System.out.println("MultiplicationTable 2");
         for (int i = 1; i < 10; i++){
             System.out.printf("2 * %d = %d\n", i, 2*i);
+        }
+
+        System.out.println("MultiplicationTable 3");
+        for (int i = 1; i < 10; i++){
+            System.out.printf("3 * %d = %d\n", i, 3*i);
+        }
+
+        System.out.println("MultiplicationTable 4");
+        for (int i = 1; i < 10; i++){
+            System.out.printf("4 * %d = %d\n", i, 4*i);
         }
     }
 }
