c736b2c0e49ceac30599a48fca05bec1755b444a
moonwonki <moonwonki1999@gmail.com>
2023-05-01 13:13:56 +0900

    feat : ForLoopForwardStep (10미만의 짝수 홀수 출력)

---
+++ src/main/java/org/example/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,13 @@
+package org.example.week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i += 2){
+            System.out.println(i);
+        }
+        System.out.println("===========================");
+        for (int i = 1; i < 10; i+= 2){
+            System.out.println(i);
+        }
+    }
+}
