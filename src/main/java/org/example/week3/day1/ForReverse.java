a195651f51b046ec8bb472cd3c7563ff3056fbae
moonwonki <moonwonki1999@gmail.com>
2023-05-01 11:21:09 +0900

    feat : ForReverse

---
+++ src/main/java/org/example/week3/day1/ForReverse.java
@@ -0,0 +1,9 @@
+package org.example.week3.day1;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0 ; i--){
+            System.out.println(i);
+        }
+    }
+}
