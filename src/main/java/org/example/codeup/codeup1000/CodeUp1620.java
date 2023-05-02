37557c391f158ac89acd9f1aae6caaad9b19f8e2
moonwonki <moonwonki1999@gmail.com>
2023-05-02 15:05:12 +0900

    feat : CodeUp1620

---
+++ src/main/java/org/example/codeup/codeup1000/CodeUp1620.java
@@ -0,0 +1,28 @@
+package org.example.codeup.codeup1000;
+
+import java.util.Scanner;
+
+public class CodeUp1620 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int numInput = scn.nextInt();
+
+        while (numInput / 10 != 0){
+            numInput = solution(numInput);
+        }
+
+        System.out.println(numInput);
+    }
+
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        while (n != 0){
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+}
