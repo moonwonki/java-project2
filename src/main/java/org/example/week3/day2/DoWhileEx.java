ce2f30cc396847e590b69febc5efd275154a8b60
moonwonki <moonwonki1999@gmail.com>
2023-05-03 10:03:12 +0900

    feat : DoWhileEx

---
+++ src/main/java/org/example/week3/day2/DoWhileEx.java
@@ -0,0 +1,11 @@
+package org.example.week3.day2;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int cnt = 0;
+        do{
+            System.out.println("do it!");
+            cnt++;
+        } while (cnt < 10);
+    }
+}
