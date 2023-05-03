3786f5d96e11ba301e89f03fa74292d7e7baf3d8
moonwonki <moonwonki1999@gmail.com>
2023-05-03 10:47:03 +0900

    feat : SumOfEvens, SumOfOdds

---
+++ src/main/java/org/example/week3/day3/SumOfEvens.java
@@ -0,0 +1,14 @@
+package org.example.week3.day3;
+
+public class SumOfEvens {
+    public static void main(String[] args) {
+        int answer = 0;
+        int num = 10;
+        for (int i = 1; i <= num; i++) {
+            if(i % 2 == 0){
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+    }
+}
