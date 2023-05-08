60e671dc9ebfb09f82ed2dcb2fc9bb4571bb67a1
moonwonki <moonwonki1999@gmail.com>
2023-05-08 14:28:50 +0900

    feat : MutsaMath

---
+++ src/main/java/org/example/week4/day1/MutsaMath.java
@@ -0,0 +1,30 @@
+package org.example.week4.day1;
+
+public class MutsaMath {
+    public static void main(String[] args) {
+        MutsaMath mutsaMath = new MutsaMath();
+        mutsaMath.printSumOfFactors(12);
+        mutsaMath.printSumOfFactors(36);
+        mutsaMath.printSumOfFactors(48);
+        mutsaMath.printSumOfFactors(29);
+    }
+
+    //method
+    public int sumOfFactors(int num){
+        int sum = 0;
+
+        int divisor = 1;
+        while(divisor <= num){
+            if (num % divisor == 0) {
+                sum += divisor;
+            }
+            divisor++;
+        }
+
+        return sum;
+    }
+
+    public void printSumOfFactors(int num){
+        System.out.printf("%d  ->  %d\n", num, sumOfFactors(num));
+    }
+}
