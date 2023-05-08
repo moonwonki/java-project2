3f01621259504dad7fdac3b88d51bc87ba296a2a
moonwonki <moonwonki1999@gmail.com>
2023-05-08 15:45:49 +0900

    feat : ArgsPrinter

---
+++ src/main/java/org/example/week4/day1/ArgsPrinter.java
@@ -0,0 +1,9 @@
+package org.example.week4.day1;
+
+import java.util.Arrays;
+
+public class ArgsPrinter {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
