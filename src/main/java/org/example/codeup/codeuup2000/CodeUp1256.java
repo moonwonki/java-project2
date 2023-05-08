50f35288c3c2a7146a89d269429ca7caed4ba6c9
moonwonki <moonwonki1999@gmail.com>
2023-05-08 09:09:40 +0900

    feat : CodeUp1256

---
+++ src/main/java/org/example/codeup/codeuup2000/CodeUp1256.java
@@ -0,0 +1,15 @@
+package org.example.codeup.codeuup2000;
+
+import java.util.Scanner;
+
+public class CodeUp1256 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int numInput = scn.nextInt();
+
+        for (int i = 0; i < numInput; i++){
+            System.out.printf("*");
+        }
+    }
+}
