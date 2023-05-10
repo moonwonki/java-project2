1480a85766e49a4435396ec1e4bec9480dcce9a9
moonwonki <moonwonki1999@gmail.com>
2023-05-10 14:41:01 +0900

    feat : ConsolePrinter2, HelloPrinter

---
+++ src/main/java/org/example/week4/day3/ConsolePrinter2.java
@@ -0,0 +1,8 @@
+package org.example.week4.day3;
+
+public class ConsolePrinter2 implements Printer2 {
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
--- src/main/java/org/example/week4/day3/HelloPrinter.java
+++ src/main/java/org/example/week4/day3/HelloPrinter.java
@@ -3,19 +3,21 @@ package org.example.week4.day3;
 public class HelloPrinter {
 
     Printer2 printer;
-    public void print(String message){
-        System.out.println(message);
+
+    public HelloPrinter(Printer2 printer) {
+        this.printer = printer;
     }
+
 
     public void repeatMessage(int n, String message) {
         for (int i = 0; i < n; i++){
-            print(message);
+            printer.print(message);
         }
     }
 
     public static void main(String[] args) {
-        HelloPrinter hp = new HelloPrinter();
-        hp.print("Hello");
-        hp.print("Bye");
+        HelloPrinter hp = new HelloPrinter(new ConsolePrinter2());
+        hp.repeatMessage(5,"Hello");
+
     }
 }
