67b402d9d1f7c1b6d955e99712c0d76b5207f44d
moonwonki <moonwonki1999@gmail.com>
2023-05-10 14:14:01 +0900

    fix : HelloPrinter class에 repeatMessage 메소드 추가

--- src/main/java/org/example/week4/day3/HelloPrinter.java
+++ src/main/java/org/example/week4/day3/HelloPrinter.java
@@ -5,6 +5,12 @@ public class HelloPrinter {
         System.out.println(message);
     }
 
+    public void repeatMessage(int n, String message) {
+        for (int i = 0; i < n; i++){
+            print(message);
+        }
+    }
+
     public static void main(String[] args) {
         HelloPrinter hp = new HelloPrinter();
         hp.print("Hello");
