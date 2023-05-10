07f72fb8efc075def4cb6e3594a3c4caf04757cc
moonwonki <moonwonki1999@gmail.com>
2023-05-10 10:30:58 +0900

    Fix : DiamondPrintALine, DiamondFileOutput

--- src/main/java/org/example/week4/day3/DiamondFileOutput.java
+++ src/main/java/org/example/week4/day3/DiamondFileOutput.java
@@ -5,6 +5,48 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class DiamondFileOutput {
+    public static void main(String[] args) throws IOException{
+        // critical logic : pivot!
+        Scanner scn = new Scanner(System.in);
+
+        int numInput = scn.nextInt();
+
+        for (int row = 0; row < numInput; row++){
+            fileOutput(makeALine(numInput, row));
+        }
+    }
+
+    public static String makeALine(int numInput, int row){
+        int pivot = numInput / 2;
+        if (row <= pivot){
+            return String.format("%s%s\n", getRepeatedSymbol(" ", numInput / 2 - row), getRepeatedSymbol("*", 2 * row + 1));
+        }
+        //역 피라미드
+        else {
+            return String.format("%s%s\n", getRepeatedSymbol(" ", row - pivot), getRepeatedSymbol("*", 2 * (numInput - row) - 1));
+        }
+    }
+
+    public static String getRepeatedSymbol(String symbol, int n){
+        return symbol.repeat(n);
+    }
+
+    public static void fileOutput(String data) throws IOException {
+        FileWriter fw = new FileWriter("./src/main/java/org/example/week4/day3/diamond.txt", true);
+        fw.write(data);
+        fw.close();
+    }
+
+    public static void consoleOutput(String data) {
+        System.out.printf("data");
+    }
+}
+
+
+
+
+
+    /*
     public static String getRepeatedSymbol(String symbol, int n){
         return symbol.repeat(n);
     }
@@ -30,4 +72,6 @@ public class DiamondFileOutput {
         }
         fw.close();
     }
-}
+
+     */
+
--- src/main/java/org/example/week4/day3/DiamondPrintALine.java
+++ src/main/java/org/example/week4/day3/DiamondPrintALine.java
@@ -9,26 +9,23 @@ public class DiamondPrintALine {
         Scanner scn = new Scanner(System.in);
 
         int numInput = scn.nextInt();
-        int pivot = numInput / 2;
+
         for (int row = 0; row < numInput; row++){
-            // 피라미드
-            if (row <= pivot){
-                System.out.printf(pyramidMakeALine(numInput, row));
-            }
-            //역 피라미드
-            else {
-                System.out.printf(reversePyramidMakeALine(numInput, pivot, row));
-            }
+            System.out.printf(makeALine(numInput, row));
         }
     }
 
-    public static String reversePyramidMakeALine(int numInput, int pivot, int row) {
-        return String.format("%s%s\n", getRepeatedSymbol(" ", row - pivot), getRepeatedSymbol("*", 2 * (numInput - row) - 1));
+    public static String makeALine(int numInput, int row){
+        int pivot = numInput / 2;
+        if (row <= pivot){
+            return String.format("%s%s\n", getRepeatedSymbol(" ", numInput / 2 - row), getRepeatedSymbol("*", 2 * row + 1));
+        }
+        //역 피라미드
+        else {
+            return String.format("%s%s\n", getRepeatedSymbol(" ", row - pivot), getRepeatedSymbol("*", 2 * (numInput - row) - 1));
+        }
     }
 
-    public static String pyramidMakeALine(int numInput, int row) {
-        return String.format("%s%s\n", getRepeatedSymbol(" ", numInput / 2 - row), getRepeatedSymbol("*", 2 * row + 1));
-    }
     public static String getRepeatedSymbol(String symbol, int n){
         return symbol.repeat(n);
     }
