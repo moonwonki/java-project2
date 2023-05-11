2dbe2dd875cd976def06784941c167c57ef30f2c
moonwonki <moonwonki1999@gmail.com>
2023-05-11 10:37:54 +0900

    feat : UserTest

---
+++ src/main/java/org/example/week4/day4/UserTest.java
@@ -0,0 +1,10 @@
+package org.example.week4.day4;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user1 = new User("abc", "000-0010-1010", 18);
+        user1.setName("Mia");
+        user1.setAge(20);
+        user1.setPhoneNumber("000-0000-0101");
+    }
+}
