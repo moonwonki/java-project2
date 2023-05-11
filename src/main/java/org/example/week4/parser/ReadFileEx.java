6fdcec5faed5c5e5de440557891ad13c939ee6c4
moonwonki <moonwonki1999@gmail.com>
2023-05-11 17:27:10 +0900

    feat : ReadFileEx

--- src/main/java/org/example/week4/parser/ReadFileEx.java
+++ src/main/java/org/example/week4/parser/ReadFileEx.java
@@ -1,10 +1,12 @@
 package org.example.week4.parser;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class ReadFileEx {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\MoonWonKi\\git\\java\\java-project2\\1.병원정보서비스 2022.10..csv"), "utf-8"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\MoonWonKi\\git\\java\\java-project2\\1.병원정보서비스 2022.10..csv"), StandardCharsets.UTF_8));
         System.out.println(br.readLine());
     }
 }
+//멋사5기식스센스팀
