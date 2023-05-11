b15c38b8cc59770cbd5073ed864d2e0cbd6ca358
moonwonki <moonwonki1999@gmail.com>
2023-05-11 10:30:19 +0900

    feat : User, ListUser

---
+++ src/main/java/org/example/week4/day4/ListUser.java
@@ -0,0 +1,12 @@
+package org.example.week4.day4;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUser {
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+    }
+}
---
+++ src/main/java/org/example/week4/day4/User.java
@@ -0,0 +1,47 @@
+package org.example.week4.day4;
+
+public class User {
+    private String name;
+    private String phoneNumber;
+    private int age;
+
+    public User(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.phoneNumber = "000";
+        this.age = -1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    boolean isAdult() {
+        return age >= 18;
+    }
+}
