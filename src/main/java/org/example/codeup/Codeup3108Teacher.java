baf7e15b06d88d1ed649f46fa8307d8d4928998c
moonwonki <moonwonki1999@gmail.com>
2023-05-11 13:44:55 +0900

    feat : Codeup3108Teacher

--- src/main/java/org/example/codeup/Codeup3108Teacher.java
+++ src/main/java/org/example/codeup/Codeup3108Teacher.java
@@ -4,37 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Codeup3108Teacher {
-    public static class Student {
-        private String code;
-        private String testId;
-        private String name;
-
-        public Student(String code, String testId, String name) {
-            this.code = code;
-            this.testId = testId;
-            this.name = name;
-        }
-
-        public String getCode() {
-            return code;
-        }
-
-        public String getTestId() {
-            return testId;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
 
     private List<Student> students = new ArrayList<>();
 
-    public Student makeAStudent(String code, String testId, String name){
+    public Student makeAStudent(String code, int testId, String name){
         return new Student(code, testId, name);
     }
 
-    public boolean isDuplicated(Student pStudent) {
+    // shift + 6
+    private boolean isExist(Student pStudent) {
         // 중복 여부 check
         for (Student student : students){
             if(pStudent.getCode().equals(student.getCode())) return true;
@@ -42,30 +20,77 @@ public class Codeup3108Teacher {
         return false;
     }
 
-    public void addAStudent(Student student){
-        if (!isDuplicated(student)){
+    private void deleteStudent(Student pStudent) {
+        for (int i = 0; i < students.size(); i++) {
+            if(pStudent.getId() == students.get(i).getId()) students.remove(i);
+        }
+    }
+
+    public void process(Student pStudent) {
+        switch (pStudent.getCode()){
+            case "I" -> addAStudent(pStudent);
+            case "D" -> deleteStudent(pStudent);
+        }
+    }
+
+    private void addAStudent(Student student){
+        if (!isExist(student)){
             students.add(student);
         }
     }
 
-    public void printStudents() {
+
+    private void printStudents() {
         for (Student student : students) {
-            System.out.printf("%s %s %s\n", student.getCode(), student.getTestId(), student.getName());
+            System.out.printf("%s %s %s\n", student.getCode(), student.getId(), student.getName());
         }
     }
-
 
     public static void main(String[] args) {
         int size = 1;
         String[] splitted = "I 1011 한라산".split(" ");
 
         Codeup3108Teacher codeup3108 = new Codeup3108Teacher();
-        Student student = codeup3108.makeAStudent(splitted[0], splitted[1], splitted[2]);
-
-
-        codeup3108.addAStudent(student);
+        Student student = codeup3108.makeAStudent(splitted[0], Integer.parseInt(splitted[1]), splitted[2]);
+        codeup3108.process(student);
         codeup3108.printStudents();
 
+    }
 
+
+    public static class Student {
+        int id;
+        String code;
+        String name;
+
+        public Student(String code, int id, String name) {
+            this.id = id;
+            this.code = code;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
