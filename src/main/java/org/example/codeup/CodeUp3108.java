20c6ab7ea43012f145ae2c182d9181301294dec1
moonwonki <moonwonki1999@gmail.com>
2023-05-11 11:32:43 +0900

    feat : CodeUp3108

---
+++ src/main/java/org/example/codeup/CodeUp3108.java
@@ -0,0 +1,76 @@
+package org.example.codeup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class CodeUp3108 {
+    static List<Student> studentList;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int numInput = Integer.parseInt(br.readLine());
+
+        studentList = new ArrayList<>();
+
+        for (int i = 0; i < numInput; i++){
+            st = new StringTokenizer(br.readLine());
+
+            Student tmp = new Student();
+            tmp.code = st.nextToken();
+            tmp.id = Integer.parseInt(st.nextToken());
+            tmp.name = st.nextToken();
+
+            executeCode(tmp);
+
+        }
+
+        studentList.sort(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                if (o1.id > o2.id) return 1;
+                else if (o1.id == o2.id) return 0;
+                else return -1;
+            }
+        });
+
+        st = new StringTokenizer(br.readLine());
+        int tokens = st.countTokens();
+        for (int i = 0; i < tokens; i++){
+            int findIndex = Integer.parseInt(st.nextToken()) - 1;
+
+            System.out.println(studentList.get(findIndex).id + " " +studentList.get(findIndex).name);
+        }
+    }
+
+    public static void executeCode(Student input){
+        if (input.code.charAt(0) == 'I' && !isContained(input.id)){
+            studentList.add(input);
+        }
+        else if (input.code.charAt(0) == 'D'){
+            for (int i = 0; i < studentList.size(); i++){
+                if (studentList.get(i).id == input.id) {
+                    studentList.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isContained(int id){
+        for (int i = 0; i < studentList.size(); i++){
+            if (studentList.get(i).id == id) return true;
+        }
+        return false;
+    }
+
+    public static class Student {
+        int id;
+        String code;
+        String name;
+    }
+}
--- src/main/java/org/example/week4/day4/ListUser.java
+++ src/main/java/org/example/week4/day4/ListUser.java
@@ -8,5 +8,15 @@ import java.util.List;
 public class ListUser {
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
+
+        User user1 = new User("Kim", "010-0101-1010", 37);
+        userList.add(user1);
+
+        User user2 = new User("Na", "010-0111-0101", 27);
+        userList.add(user2);
+
+        for (User user: userList){
+            System.out.printf("%s's phone number is %s\n", user.getName(), user.getPhoneNumber());
+        }
     }
 }
