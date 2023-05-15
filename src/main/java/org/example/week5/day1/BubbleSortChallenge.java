eea15924909cc3e791834ac5135f21596b99659b
moonwonki <moonwonki1999@gmail.com>
2023-05-15 10:21:51 +0900

    feat : BubbleSortChallenge

--- src/main/java/org/example/week5/day1/BubbleSortChallenge.java
+++ src/main/java/org/example/week5/day1/BubbleSortChallenge.java
@@ -3,11 +3,16 @@ package org.example.week5.day1;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class BubbleSort01 {
+public class BubbleSortChallenge {
+    private Comparator<Integer> comparator;
+
+    public BubbleSortChallenge(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
 
     public int[] sortARound(int[] arr, int until){
         for (int i = 0; i < until; i++) {
-            if (arr[i] > arr[i + 1]) {
+            if (comparator.compare(arr[i], arr[i + 1]) > 0) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
@@ -24,7 +29,13 @@ public class BubbleSort01 {
     }
 
     public static void main(String[] args) {
-        BubbleSort01 bubbleSort02 = new BubbleSort01();
+        BubbleSortChallenge bubbleSort02 = new BubbleSortChallenge(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
         int[] arr = {7, 2, 3, 9, 28, 1};
         arr = bubbleSort02.sort(arr);
         System.out.println(Arrays.toString(arr));
