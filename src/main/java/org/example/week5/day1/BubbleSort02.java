568a8bce0ccd4c5aa2340a916134f6cbd997eed0
moonwonki <moonwonki1999@gmail.com>
2023-05-15 10:50:38 +0900

    feat : BubbleSort02 개선

--- src/main/java/org/example/week5/day1/BubbleSort02.java
+++ src/main/java/org/example/week5/day1/BubbleSort02.java
@@ -1,21 +1,42 @@
 package org.example.week5.day1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BubbleSort02 {
+    Comparator<Integer> ascendCp = (o1, o2) -> o1 - o2;
+
+    Comparator<Integer> descendCp = (o1, o2) -> o2 - o1;
+
     public static void main(String[] args) {
         int[] arr = {7, 2, 3, 9, 28, 1};
+        BubbleSort02 bs = new BubbleSort02();
+
+        arr = bs.sortArray(arr, false);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private int[] sortArray(int[] arr, boolean isAscend) {
 
         for (int i = 0; i < arr.length - 1; i++){
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (isAscend) {
+                    if (ascendCp.compare(arr[i], arr[j]) > 0) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                else {
+                    if (descendCp.compare(arr[i], arr[j]) > 0) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
             }
         }
-
-        System.out.println(Arrays.toString(arr));
+        return arr;
     }
 }
