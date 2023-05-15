5f15f162737924afcaf5d137a124f1b4c140024f
moonwonki <moonwonki1999@gmail.com>
2023-05-15 10:40:58 +0900

    feat : BubbleSort02 개선

--- src/main/java/org/example/week5/day1/BubbleSort02.java
+++ src/main/java/org/example/week5/day1/BubbleSort02.java
@@ -6,14 +6,16 @@ public class BubbleSort02 {
     public static void main(String[] args) {
         int[] arr = {7, 2, 3, 9, 28, 1};
 
-        int i = 0;
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
+
         System.out.println(Arrays.toString(arr));
     }
 }
